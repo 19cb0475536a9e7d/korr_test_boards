a4fc0235f03d68aebeaf9c5f38d6a6661247a2d0
Oleg Shishlyannikov <oleg.shishlyannikov.1992@gmail.com>
2020-02-26 16:46:11 +0300

    Fix

--- software/signal_board/port/korr_test_board/include/board.h
+++ software/signal_board/port/korr_test_board/include/board.h
@@ -1,73 +1,62 @@
 #ifndef __BOARDS_ARM_STM32_KORR_TEST_SIGNAL_BOARD_INCLUDE_BOARD_H
 #define __BOARDS_ARM_STM32_KORR_TEST_SIGNAL_BOARD_INCLUDE_BOARD_H
 
-#include <nuttx/config.h>
 #include <stdint.h>
 
 /* Clocking */
 /* On-board crystal frequency is 8MHz (HSE) */
 static constexpr uint64_t STM32_BOARD_XTAL = 8000000ul;
-#define STM32_BOARD_XTAL        8000000ul
 
 /* PLL source is HSE/1, PLL multiplier is 9: PLL frequency is 8MHz (XTAL) x 9 = 72MHz */
 static constexpr uint32_t STM32_CFGR_PLLSRC = RCC_CFGR_PLLSRC;
-#define STM32_CFGR_PLLSRC       RCC_CFGR_PLLSRC
-#define STM32_CFGR_PLLXTPRE     0
-#define STM32_CFGR_PLLMUL       RCC_CFGR_PLLMUL_CLKx9
-#define STM32_PLL_FREQUENCY     (9*STM32_BOARD_XTAL)
+static constexpr uint32_t STM32_CFGR_PLLXTPRE = 0;
+static constexpr uint32_t STM32_CFGR_PLLMUL = RCC_CFGR_PLLMUL_CLKx9;
+static constexpr uint32_t STM32_PLL_FREQUENCY = (9 * STM32_BOARD_XTAL);
 
 /* Use the PLL and set the SYSCLK source to be the PLL */
-
-#define STM32_SYSCLK_SW         RCC_CFGR_SW_PLL
-#define STM32_SYSCLK_SWS        RCC_CFGR_SWS_PLL
-#define STM32_SYSCLK_FREQUENCY  STM32_PLL_FREQUENCY
+static constexpr uint32_t STM32_SYSCLK_SW = RCC_CFGR_SW_PLL;
+static constexpr uint32_t STM32_SYSCLK_SWS = RCC_CFGR_SWS_PLL;
+static constexpr uint32_t STM32_SYSCLK_FREQUENCY = STM32_PLL_FREQUENCY;
 
 /* AHB clock (HCLK) is SYSCLK (72MHz) */
-
-#define STM32_RCC_CFGR_HPRE     RCC_CFGR_HPRE_SYSCLK
-#define STM32_HCLK_FREQUENCY    STM32_PLL_FREQUENCY
-#define STM32_BOARD_HCLK        STM32_HCLK_FREQUENCY    /* same as above, to satisfy compiler */
+static constexpr uint32_t STM32_RCC_CFGR_HPRE = RCC_CFGR_HPRE_SYSCLK;
+static constexpr uint32_t STM32_HCLK_FREQUENCY = STM32_PLL_FREQUENCY;
+static constexpr uint32_t STM32_BOARD_HCLK = STM32_HCLK_FREQUENCY;
 
 /* APB2 clock (PCLK2) is HCLK (72MHz) */
-
-#define STM32_RCC_CFGR_PPRE2    RCC_CFGR_PPRE2_HCLK
-#define STM32_PCLK2_FREQUENCY   STM32_HCLK_FREQUENCY
+static constexpr uint32_t STM32_RCC_CFGR_PPRE2 = RCC_CFGR_PPRE2_HCLK;
+static constexpr uint32_t STM32_PCLK2_FREQUENCY = STM32_HCLK_FREQUENCY;
 
 /* APB2 timers 1 and 8 will receive PCLK2. */
-
-#define STM32_APB2_TIM1_CLKIN   (STM32_PCLK2_FREQUENCY)
-#define STM32_APB2_TIM8_CLKIN   (STM32_PCLK2_FREQUENCY)
+static constexpr uint32_t STM32_APB2_TIM1_CLKIN = (STM32_PCLK2_FREQUENCY);
+static constexpr uint32_t STM32_APB2_TIM8_CLKIN = (STM32_PCLK2_FREQUENCY);
 
 /* APB1 clock (PCLK1) is HCLK/2 (36MHz) */
-
-#define STM32_RCC_CFGR_PPRE1    RCC_CFGR_PPRE1_HCLKd2
-#define STM32_PCLK1_FREQUENCY   (STM32_HCLK_FREQUENCY/2)
+static constexpr uint32_t STM32_RCC_CFGR_PPRE1 = RCC_CFGR_PPRE1_HCLKd2;
+static constexpr uint32_t STM32_PCLK1_FREQUENCY = (STM32_HCLK_FREQUENCY / 2);
 
 /* APB1 timers 2-7 will be twice PCLK1 */
-
-#define STM32_APB1_TIM2_CLKIN   (2*STM32_PCLK1_FREQUENCY)
-#define STM32_APB1_TIM3_CLKIN   (2*STM32_PCLK1_FREQUENCY)
-#define STM32_APB1_TIM4_CLKIN   (2*STM32_PCLK1_FREQUENCY)
-#define STM32_APB1_TIM5_CLKIN   (2*STM32_PCLK1_FREQUENCY)
-#define STM32_APB1_TIM6_CLKIN   (2*STM32_PCLK1_FREQUENCY)
-#define STM32_APB1_TIM7_CLKIN   (2*STM32_PCLK1_FREQUENCY)
+static constexpr uint32_t STM32_APB1_TIM2_CLKIN = (2 * STM32_PCLK1_FREQUENCY);
+static constexpr uint32_t STM32_APB1_TIM3_CLKIN = (2 * STM32_PCLK1_FREQUENCY);
+static constexpr uint32_t STM32_APB1_TIM4_CLKIN = (2 * STM32_PCLK1_FREQUENCY);
+static constexpr uint32_t STM32_APB1_TIM5_CLKIN = (2 * STM32_PCLK1_FREQUENCY);
+static constexpr uint32_t STM32_APB1_TIM6_CLKIN = (2 * STM32_PCLK1_FREQUENCY);
+static constexpr uint32_t STM32_APB1_TIM7_CLKIN = (2 * STM32_PCLK1_FREQUENCY);
 
 /* USB divider -- Divide PLL clock by 1.5 */
-
-#define STM32_CFGR_USBPRE       0
+static constexpr uint32_t STM32_CFGR_USBPRE = 0;
 
 /* Timer Frequencies, if APBx is set to 1, frequency is same to APBx
  * otherwise frequency is 2xAPBx.
  * Note: TIM1,8 are on APB2, others on APB1 */
-
-#define BOARD_TIM1_FREQUENCY    STM32_HCLK_FREQUENCY
-#define BOARD_TIM2_FREQUENCY    STM32_PCLK1_FREQUENCY
-#define BOARD_TIM3_FREQUENCY    STM32_PCLK1_FREQUENCY
-#define BOARD_TIM4_FREQUENCY    STM32_PCLK1_FREQUENCY
-#define BOARD_TIM5_FREQUENCY    STM32_PCLK1_FREQUENCY
-#define BOARD_TIM6_FREQUENCY    STM32_PCLK1_FREQUENCY
-#define BOARD_TIM7_FREQUENCY    STM32_PCLK1_FREQUENCY
-#define BOARD_TIM8_FREQUENCY    STM32_HCLK_FREQUENCY
+static constexpr uint32_t BOARD_TIM1_FREQUENCY = STM32_HCLK_FREQUENCY;
+static constexpr uint32_t BOARD_TIM2_FREQUENCY = STM32_PCLK1_FREQUENCY;
+static constexpr uint32_t BOARD_TIM3_FREQUENCY = STM32_PCLK1_FREQUENCY;
+static constexpr uint32_t BOARD_TIM4_FREQUENCY = STM32_PCLK1_FREQUENCY;
+static constexpr uint32_t BOARD_TIM5_FREQUENCY = STM32_PCLK1_FREQUENCY;
+static constexpr uint32_t BOARD_TIM6_FREQUENCY = STM32_PCLK1_FREQUENCY;
+static constexpr uint32_t BOARD_TIM7_FREQUENCY = STM32_PCLK1_FREQUENCY;
+static constexpr uint32_t BOARD_TIM8_FREQUENCY = STM32_HCLK_FREQUENCY;
 
 /* SDIO dividers.  Note that slower clocking is required when DMA is disabled
  * in order to avoid RX overrun/TX underrun errors due to delayed responses
@@ -76,32 +65,29 @@ static constexpr uint32_t STM32_CFGR_PLLSRC = RCC_CFGR_PLLSRC;
  *
  * HCLK=72MHz, SDIOCLK=72MHz, SDIO_CK=HCLK/(178+2)=400 KHz
  */
-
-#define SDIO_INIT_CLKDIV        (178 << SDIO_CLKCR_CLKDIV_SHIFT)
+static constexpr uint32_t SDIO_INIT_CLKDIV = (178u << SDIO_CLKCR_CLKDIV_SHIFT);
 
 /* DMA ON:  HCLK=72 MHz, SDIOCLK=72MHz, SDIO_CK=HCLK/(2+2)=18 MHz
  * DMA OFF: HCLK=72 MHz, SDIOCLK=72MHz, SDIO_CK=HCLK/(3+2)=14.4 MHz
  */
-
 #ifdef CONFIG_SDIO_DMA
-#  define SDIO_MMCXFR_CLKDIV    (2 << SDIO_CLKCR_CLKDIV_SHIFT)
+#  define SDIO_MMCXFR_CLKDIV (2u << SDIO_CLKCR_CLKDIV_SHIFT)
 #else
-#  define SDIO_MMCXFR_CLKDIV    (3 << SDIO_CLKCR_CLKDIV_SHIFT)
+#  define SDIO_MMCXFR_CLKDIV (3u << SDIO_CLKCR_CLKDIV_SHIFT)
 #endif
 
 /* DMA ON:  HCLK=72 MHz, SDIOCLK=72MHz, SDIO_CK=HCLK/(1+2)=24 MHz
  * DMA OFF: HCLK=72 MHz, SDIOCLK=72MHz, SDIO_CK=HCLK/(3+2)=14.4 MHz
  */
-
 #ifdef CONFIG_SDIO_DMA
-#  define SDIO_SDXFR_CLKDIV     (1 << SDIO_CLKCR_CLKDIV_SHIFT)
+#  define SDIO_SDXFR_CLKDIV (1 << SDIO_CLKCR_CLKDIV_SHIFT)
 #else
-#  define SDIO_SDXFR_CLKDIV     (3 << SDIO_CLKCR_CLKDIV_SHIFT)
+#  define SDIO_SDXFR_CLKDIV (3 << SDIO_CLKCR_CLKDIV_SHIFT)
 #endif
 
 /* BUTTON definitions ***************************************************************/
 
-enum user_buttons_e {
+enum struct user_buttons_es : uint32_t {
   BUTTON_USER1 = 0,
   BUTTON_USER2 = 1,
   BUTTON_USER3 = 2,
@@ -113,6 +99,6 @@ enum user_buttons_e {
 
 /* LED definitions ******************************************************************/
 
-enum user_leds_e { USER_LED0 = 0, USER_LED1 = 1, USER_LED2 = 2, USER_LED3 = 3, BOARD_NLEDS = 4 };
+enum struct user_leds_es : uint32_t { USER_LED0 = 0, USER_LED1 = 1, USER_LED2 = 2, USER_LED3 = 3, BOARD_NLEDS = 4 };
 
 #endif /* __BOARDS_ARM_STM32_KORR_TEST_SIGNAL_BOARD_INCLUDE_BOARD_H */
